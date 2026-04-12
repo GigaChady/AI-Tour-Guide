25e55720aaeb96510ebbc6fb8bab5983e3ccde78
Jakub Gryko <grykoj@outlook.com>
2026-04-13 00:42:17 +0200

    [mobile][ui] move api client config to AppConfig

--- Mobile/android/app/src/main/java/ai/tour/guide/config/AppConfig.kt
+++ Mobile/android/app/src/main/java/ai/tour/guide/config/AppConfig.kt
@@ -1,6 +1,10 @@
 package ai.tour.guide.config
 
+import io.ktor.http.URLProtocol
+
 object AppConfig {
-    val SIGN_IN_WITH_GOOGLE_CLIENT_ID =
+    const val SIGN_IN_WITH_GOOGLE_CLIENT_ID =
         "934881955828-0221kagjsgdefr5ttkp4vh2dv7unrlrg.apps.googleusercontent.com"
+    val HTTPS_CLIENT_PROTOCOL: URLProtocol = URLProtocol.HTTP
+    const val HTTPS_CLIENT_HOST: String = "localhost:8000"
 }
