0f664cf3de1526374dfc979ae642a4460eccb174
Naciej <maciej.piotrowski.business@gmail.com>
2026-04-13 11:17:23 +0200

    feat: add WIP main method for AI narration generation pipeline

--- AI/main.py
+++ AI/main.py
@@ -1,14 +1,7 @@
 import logging
-import os
-import json
 
-from pipeline.tour_narration_pipeline_factory import TourNarrationPipelineFactory
-from processors.narration_processor import NarrationProcessor
-from processors.photo_processor import PhotoProcessor
-from storage.minio_image_storage import MinioImageStorage
-from connections.redis_stream_worker import RedisStreamWorker
-from processors.backend_processor import BackendProcessor
-from schemas import NarrationDetailLevel, NarrationLanguage, NarrationSettings
+from AI.narration.narration_manager import NarrationManager
+from AI.narration.common.narration_settings import NarrationSettings, NarrationDetailLevel, NarrationLanguage
 
 if __name__ == "__main__":
     # Logging config (debug)
@@ -18,23 +11,18 @@ if __name__ == "__main__":
         datefmt='%Y-%m-%d %H:%M:%S'
     )
 
-    if os.getenv("AI_RUN_STREAM_WORKER", "1") == "1":
+    # Narration settings
+    narration_settings = NarrationSettings(
+        latitude=41.889799,
+        longitude=12.491015,
+        detail_level=NarrationDetailLevel.DETAILED,
+        search_radius=50,
+        language=NarrationLanguage(language_name="polski", language_tag="pl"),
+        user_preferences="history architecture"
+    )
 
-        photo_processor = PhotoProcessor(MinioImageStorage())
-        narration_processor = NarrationProcessor(photo_processor=photo_processor)
-        backend_processor = BackendProcessor(narration_processor=narration_processor)
-        stream_worker = RedisStreamWorker(backend_processor)
-        stream_worker.run()
-        
-    else:
-        narration_settings = NarrationSettings(
-            latitude=41.889799,
-            longitude=12.491015,
-            detail_level=NarrationDetailLevel.DETAILED,
-            search_radius=50,
-            language=NarrationLanguage(language_name="Polish", language_tag="pl"),
-            user_preferences="history architecture",
-        )
+    narration_manager = NarrationManager.build_narration_manager(narration_settings)
 
-        result = TourNarrationPipelineFactory().create(narration_settings).run()
-        print(json.dumps(result.model_dump(), ensure_ascii=False, indent=2))
+    # Generate narration
+    narration = narration_manager.get_narration()
+    print(narration)
