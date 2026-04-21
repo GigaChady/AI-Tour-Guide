77c723db2cabeb7efaf022a390c8e1f9ed50638f
Michał <michal.sachanbinski@gmail.com>
2026-04-21 18:35:04 +0200

    fix: Communication between Backend and AI services.

--- AI/connections/configs/redis_config.py
+++ AI/connections/configs/redis_config.py
@@ -7,6 +7,7 @@ DEFAULT_REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379/0")
 DEFAULT_BLOCK_MS = int(os.getenv("AI_STREAM_BLOCK_MS", "5000"))
 DEFAULT_COUNT = int(os.getenv("AI_STREAM_COUNT", "10"))
 DEFAULT_AI_MOCK = bool(os.getenv("AI_MOCK", True))
+DEFAULT_PREF_CACHE = str(os.getenv("REDIS_PREF_CACHE", "preferences:"))
 
 
 @dataclass(frozen=True) # immutable
@@ -17,5 +18,6 @@ class RedisWorkerConfig:
     redis_url: str = DEFAULT_REDIS_URL
     block_ms: int = DEFAULT_BLOCK_MS
     count: int = DEFAULT_COUNT
+    pref_cache:str = DEFAULT_PREF_CACHE
     ai_mock: bool = DEFAULT_AI_MOCK
     start_id: str = "$"
