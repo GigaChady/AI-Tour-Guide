001755db5a3fbd3f9862483dbf84260ba330574f
Maciej Piotrowski <maciej.piotrowski.business@gmail.com>
2026-05-03 23:04:45 +0100

    feat: add app theme support

--- Mobile/android/app/src/main/java/ai/tour/guide/activity/MainActivity.kt
+++ Mobile/android/app/src/main/java/ai/tour/guide/activity/MainActivity.kt
@@ -18,6 +18,7 @@ import org.koin.android.ext.android.inject
 class MainActivity : ComponentActivity() {
     val appDataRepository: AppDataRepository by inject()
 
+    // TODO: Refactor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
