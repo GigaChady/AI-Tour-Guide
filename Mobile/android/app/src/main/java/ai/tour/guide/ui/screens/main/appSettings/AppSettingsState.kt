9285abd0dfe8aa3127fa7cf011479ea770211b6b
Maciej Piotrowski <maciej.piotrowski.business@gmail.com>
2026-05-05 22:48:02 +0200

    refactor(settings): update default language and speed and remove volume property

--- Mobile/android/app/src/main/java/ai/tour/guide/ui/screens/main/appSettings/AppSettingsState.kt
+++ Mobile/android/app/src/main/java/ai/tour/guide/ui/screens/main/appSettings/AppSettingsState.kt
@@ -6,10 +6,9 @@ import ai.tour.guide.data.appSettings.AppSettingsDetailLevelType
 
 data class AppSettingsState(
     val appTheme: AppSettingsAppThemeType = AppSettingsAppThemeType.SYSTEM,
-    val language: String = "pl",
+    val language: String = "en",
     val pitch: Float = 50f,
-    val speed: Float = 50f,
-    val volume: Float = 20f, // Not used in View (AppSettingsScreen) TODO: Decide whether this option should be kept
+    val speed: Float = 5f,
     val detailLevel: AppSettingsDetailLevelType = AppSettingsDetailLevelType.MEDIUM,
     val autoPlay: Boolean = true
 ) {
