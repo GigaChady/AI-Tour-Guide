de307ae8da3907f38d275eeef6c638710d328f1d
Jakub Gryko <grykoj@outlook.com>
2026-05-07 13:25:04 +0200

    [mobile][tour] POI saving improvements

--- Mobile/android/app/src/main/java/ai/tour/guide/network/schema/response/RoutePOIDto.kt
+++ Mobile/android/app/src/main/java/ai/tour/guide/network/schema/response/RoutePOIDto.kt
@@ -1,10 +1,13 @@
 package ai.tour.guide.network.schema.response
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class RoutePOIDto(
     val type: String,
+    @SerialName("narration_id")
+    val narrationId: String? = null,
     val data: List<ReceivedRoutePOI>
 )
 
