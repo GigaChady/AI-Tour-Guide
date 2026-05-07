7d4cf91ebf5ef219757034c54f4d20a55f3fad57
Naciej <maciej.piotrowski.business@gmail.com>
2026-05-07 23:12:02 +0200

    refactor(settings): move DTO and state mapping logic to extension functions

--- Mobile/android/app/src/main/java/ai/tour/guide/network/schema/response/AppSettingsResponseDto.kt
+++ Mobile/android/app/src/main/java/ai/tour/guide/network/schema/response/AppSettingsResponseDto.kt
@@ -1,6 +1,8 @@
 package ai.tour.guide.network.schema.response
 
+import ai.tour.guide.data.appSettings.AppSettingsAppThemeType
 import ai.tour.guide.data.appSettings.AppSettingsDetailLevelType
+import ai.tour.guide.data.appSettings.AppSettingsDto
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -21,3 +23,15 @@ data class AppSettingsResponseDto(
 
     override val detail: String? = null
 ) : IAPIResponseDto
+
+// AppSettingsResponseDto needs conversion to AppSettingsDto before further usage
+fun AppSettingsResponseDto.toDto(currentTheme: AppSettingsAppThemeType? = null): AppSettingsDto {
+    return AppSettingsDto(
+        appTheme = currentTheme, // Added from local storage
+        language = this.language,
+        pitch = this.pitch,
+        speed = this.speed,
+        detailLevel = this.detailLevel,
+        autoPlay = this.autoPlay
+    )
+}
