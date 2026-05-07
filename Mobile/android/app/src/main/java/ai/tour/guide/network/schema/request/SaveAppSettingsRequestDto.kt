f69984d8433b4995d858949ca889cb6f6e8430c9
Naciej <maciej.piotrowski.business@gmail.com>
2026-05-07 22:58:32 +0200

    refactor(settings): remove redundant @SerialName annotations

--- Mobile/android/app/src/main/java/ai/tour/guide/network/schema/request/SaveAppSettingsRequestDto.kt
+++ Mobile/android/app/src/main/java/ai/tour/guide/network/schema/request/SaveAppSettingsRequestDto.kt
@@ -6,9 +6,16 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class SaveAppSettingsRequestDto(
-    @SerialName("language") val language: String? = null,
-    @SerialName("pitch") val pitch: Int? = null,
-    @SerialName("speed") val speed: Int? = null,
-    @SerialName("detail_level") val detailLevel: AppSettingsDetailLevelType? = null,
-    @SerialName("auto_play") val autoPlay: Boolean? = null
+
+    val language: String? = null,
+
+    val pitch: Int? = null,
+
+   val speed: Int? = null,
+
+    @SerialName("detail_level")
+    val detailLevel: AppSettingsDetailLevelType? = null,
+
+    @SerialName("auto_play")
+    val autoPlay: Boolean? = null
 )
