be7e817b3ab757e0c1436812221f66e8401915f7
Jakub Gryko <grykoj@outlook.com>
2026-05-10 03:02:23 +0200

    [mobile][tour] adding option to skip to next/prev tour

--- Mobile/android/app/src/main/java/ai/tour/guide/data/room/entity/RouteStop.kt
+++ Mobile/android/app/src/main/java/ai/tour/guide/data/room/entity/RouteStop.kt
@@ -6,6 +6,7 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import kotlin.random.Random
 
 @Entity(
     tableName = "stops",
@@ -30,7 +31,9 @@ data class RouteStop(
     @ColumnInfo(name = "narration_id")
     val serverNarrationId: String? = null,
     @ColumnInfo(name = "location_title")
-    val locationTitle: String? = null,
+    val locationTitle: String? = "Lokalizacja ${Random.nextInt(0, 100000)}",
+    @ColumnInfo(name = "location_image")
+    val locationImage: String? = "https://picsum.photos/1280/720",
     @ColumnInfo(name = "narration_string")
     val narrationString: String? = null,
     @ColumnInfo(name = "narration_words_map")
